9f7901b6a1f9bc250adecd2367f22c53c797288c
“Naty-S” <na7a5cha010@gmail.com>
2025-06-26 19:55:05 -0400

    scaffolding

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
-
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use solana_sdk;
 
 #[cfg(test)]
 mod tests {
-    use solana_sdk;
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+  #[test]
+  fn keygen() {
+  }
+
+  #[test]
+  fn airdrop() {
+  }
+
+  #[test]
+  fn transfer_sol() {
     }
 }
