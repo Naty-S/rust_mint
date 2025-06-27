6698de4a816779058ccf42485ada279080642ba7
“Naty-S” <na7a5cha010@gmail.com>
2025-06-26 21:11:04 -0400

    airdrop to dev wallet

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
-use solana_sdk::{
-    signature::{Keypair, Signer},
-    pubkey::Pubkey
-};
-
 #[cfg(test)]
 mod tests {
-  use super::*;
+  use solana_sdk::{
+      signature::{Keypair, Signer, read_keypair_file},
+      // pubkey::Pubkey
+  };
+  use solana_client::rpc_client::RpcClient;
+  
+  pub const RPC_URL: &str = "https://turbine-solanad-4cde.devnet.rpcpool.com/9a9da9cf-6db1-47dc-839a-55aca5c9c80a";
 
   #[test]
   fn keygen() {
@@ -18,9 +19,21 @@ mod tests {
 
   #[test]
   fn airdrop() {
+    // Import KP, if not expect then type got Err in it and not Keypair type
+    let keypair = read_keypair_file("src/wallets/dev-wallet.json").expect("Couldn't find wallet file");
+    // Connect to Solana devnet
+    let client = RpcClient::new(RPC_URL);
+    // Claim 2 devnet SOL (2 billion lamports)
+    match client.request_airdrop(&keypair.pubkey(), 2_000_000_000u64) {
+      Ok(sig) => {
+        println!("Success! Check your TX here:");
+        println!("https://explorer.solana.com/tx/{}?cluster=devnet", sig);
+      }
+      Err(err) => { println!("Airdrop failed: {}", err); }
+    }
   }
 
   #[test]
   fn transfer_sol() {
-    }
+  }
 }
